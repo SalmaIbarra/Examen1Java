07995857f291f7819fe76bcf37f1d7d668c11b7a
SALMA YARETH IBARRA FLORES <2020030315@upsin.edu.mx>
2023-06-02 12:42:58 -0700

    Se programo y termino el CuentaBancoActivity

---
+++ app/src/main/java/com/example/examen1java/Activit_cuenta_banco.java
@@ -0,0 +1,38 @@
+package com.example.examen1java;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CuentaBancoActivity extends AppCompatActivity {
+
+    private TextView lblNombre;
+    private TextView lblSaldo;
+    private TextView lblBanco;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activit_cuenta_banco);
+
+        iniciarComponentes();
+
+        String nombre = getIntent().getStringExtra("nombre");
+        String banco = getIntent().getStringExtra("banco");
+        String saldo = getIntent().getStringExtra("saldo");
+
+        lblNombre.setText(nombre);
+        lblBanco.setText(banco);
+        lblSaldo.setText(saldo);
+    }
+
+    private void iniciarComponentes() {
+        lblNombre = findViewById(R.id.lblNombreCliente);
+        lblBanco = findViewById(R.id.lblBanco);
+        lblSaldo = findViewById(R.id.lblSaldo);
+    }
+}
+
+
+    }
+}
